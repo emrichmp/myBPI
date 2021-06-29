152b06fd5d204fda3feaf2af74b87303d91070d2
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-29 13:55:54 -0400

    started form

--- src/Form/Inputs.js
+++ src/Form/Inputs.js
@@ -4,7 +4,20 @@ class Inputs extends React.Component {
     render(){
         return(
             <div>
-                
+                <form>
+                    <label>Start Date:</label>
+                    <input type="date"></input>
+                    <label>End Date:</label>
+                    <input type="date"></input>
+                    <label>Currency</label>
+                    <select name="currency">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                    <button>Pull Data</button>
+                </form>
             </div>
         )
     }
