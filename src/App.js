d1f2e3bcbd3358dfea016c19af41a535c9d79afb
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-29 12:41:01 -0400

    created header component for the top

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Inputs from './Form/Inputs'
+import Header from './Header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Inputs />
     </div>
   );
