a79f00067e71924d73b7589241b9a092b92a9fe3
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-29 12:55:50 -0400

    moved logo to header folder, imported and displayed logo in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from './logo-flower.png'
 
 class Header extends React.Component {
     render(){
         return(
             <div>
+                <img src={logo} alt="" />
                 <a href="https://www.vian.ai/contact-us/">Contact Us</a>
             </div>
         )
