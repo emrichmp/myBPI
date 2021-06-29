631822f54a660d4e5021007b66a81fe90f675f40
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-29 13:37:54 -0400

    removed text from input comp

--- src/Form/Inputs.js
+++ src/Form/Inputs.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Inputs extends React.Component {
     render(){
         return(
-            <div>form</div>
+            <div>
+                
+            </div>
         )
     }
 }
