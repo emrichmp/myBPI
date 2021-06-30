f8b13d88d15b887e4b7d499c750aab580b863542
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-30 19:12:43 -0400

    styled current data

--- src/DataDisplay/CurrentDisplay.js
+++ src/DataDisplay/CurrentDisplay.js
@@ -1,4 +1,5 @@
 import he from 'he';
+import './Current.css'
 
 function CurrentDisplay(props){
     //presentation component that takes in prop data from Current.js and displays it on page
@@ -6,12 +7,12 @@ function CurrentDisplay(props){
     console.log(props.data.bpi)
     if (props.data.bpi !== undefined){
         console.log("yeet")
-        return <div className="current-display">
-                <h1>Current BPI Data:</h1>
+        return <div>
+                <h1 className="current-display" >Current BitCoin Values</h1>
                     {Object.entries(props.data.bpi).map(([key, value]) => {
                         // Pretty straightforward - use key for the key and value for the value.
                         // Just to clarify: unlike object destructuring, the parameter names don't matter here.
-                        return <h3 key={key}> {value.code} {he.decode(value.symbol)} {value.rate} </h3>
+                        return <h3 className="current-data" key={key}> {value.code} {he.decode(value.symbol)} {value.rate} </h3>
                     })}
              </div>
     } else {
