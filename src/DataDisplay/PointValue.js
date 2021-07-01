66e6ca1408ee3106ab3c0d05a7716b9164e58924
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 17:31:49 -0400

    commented PointValue to document its purpose and were it gets its data

--- src/DataDisplay/PointValue.js
+++ src/DataDisplay/PointValue.js
@@ -1,5 +1,6 @@
 import './Graph.css'
 
+//Takes in data from LineGraphData and displays the date and value of the current hovered point
 function PointValue(props){
     const date = new Date(props.point[0][0]).toLocaleDateString("en-US")
     return <div className="pointval">Date:{date} <br></br> Value: ${parseInt(props.point[0][1])}</div>
