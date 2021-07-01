c132aa73478767d1d53ec3583b3ca72c8979513f
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-30 20:47:50 -0400

    removed table console.logs

--- src/DataDisplay/Table.js
+++ src/DataDisplay/Table.js
@@ -2,9 +2,6 @@ import './Table.css'
 
 function Table(props){
     //Accepts data from Form/Input.js as props and displays BitCoin History Data in table form
-    console.log(props.data)
-    console.log(props.start)
-    console.log(props.end)
     if (props.data.bpi !== undefined){
         return <div>
                     <h2 className="table-header" > Showing data from {props.start} to {props.end} </h2>
