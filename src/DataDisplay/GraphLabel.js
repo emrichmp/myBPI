8dc2156eff42d88bfc2de17d1164e467d5cfabf5
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 17:29:49 -0400

    commented graphlabel to document its purpose

--- src/DataDisplay/GraphLabel.js
+++ src/DataDisplay/GraphLabel.js
@@ -1,6 +1,7 @@
 import './Graph.css'
 
-function GraphLabel(props){
+//Very simple, just labels the graph
+function GraphLabel(){
     return <div>
             <h3 className="y-axis">Value</h3>
             <h3 className="x-axis">Date</h3>
