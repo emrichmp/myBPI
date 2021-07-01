da21715ecc7f07ce6cbab997f42ff5ce9e97aecc
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 17:05:23 -0400

    switched x and y axis

--- src/DataDisplay/GraphLabel.js
+++ src/DataDisplay/GraphLabel.js
@@ -2,8 +2,8 @@ import './Graph.css'
 
 function GraphLabel(props){
     return <div>
-            <h3 className="y-axis">Date</h3>
-            <h3 className="x-axis">Value</h3>
+            <h3 className="y-axis">Value</h3>
+            <h3 className="x-axis">Date</h3>
             </div>
 }
 
