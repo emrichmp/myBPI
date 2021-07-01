b5b85a3aef233cf2486d2c977547064798e8d9c5
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 09:48:38 -0400

    added comments to profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+//Component allows user to update profile data
+//This page would theoretically pull from a backend that handles login
+//User can update name, birthday, email and phone number that associated with a theoretical account
+//Uses redux to handle state of profile information
 class Profile extends React.Component{
     render(){
         return(
