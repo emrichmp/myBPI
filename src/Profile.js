d0a70651fb3dd93d03ce5643e7f07d41ecdb432e
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 17:48:45 -0400

    started profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './App.css'
 
 //Component allows user to update profile data
 //This page would theoretically pull from a backend that handles login
 //User can update name, birthday, email and phone number that associated with a theoretical account
 //Uses redux to handle state of profile information
 class Profile extends React.Component{
+
+    state = {
+        name: "User",
+        email: "email@vian.ai",
+        pn: "917-123-4567"
+    }
+
     render(){
         return(
-            <div className="profile-comp"><h1>PROFILE</h1></div>
+            <div className="profile-comp">
+                <h1>PROFILE</h1>
+                <h3>Name: {this.state.name}</h3>
+                <h3>Email: {this.state.email}</h3>
+                <h3>Phone Number: {this.state.pn}</h3>
+            </div>
         )
     }
 }
