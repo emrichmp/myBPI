5dd658a372ce207d804d2508863969929a9bbfb3
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-30 22:24:46 -0400

    added react-router for profile link and home link, changed styling a bit, still need to solve profile problem

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Inputs from './Form/Inputs'
 import Header from './Header/Header'
 import Current from './DataDisplay/Current'
 import Blurb from './DataDisplay/Blurb'
+import {
+  Route,
+  HashRouter
+} from "react-router-dom";
+import Profile from './Profile'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Blurb />
-      <Current />
-      <Inputs />
+      <HashRouter>
+        <Header />
+        <Route exact path="/" component={Blurb} />
+        <Route exact path="/" component={Current} />
+        <Route exact path="/" component={Inputs} />
+        <Route exact path="/profile" componenet={Profile} />
+      </HashRouter>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo-flower.png'
 import './Header.css';
+import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
     //Created header to look very similar to Vianai website, contact link leads to Vianai, disclaimer is required for utilization of coindesk
@@ -11,6 +12,8 @@ class Header extends React.Component {
                 <img className="logo" src={logo} alt="" />
                 <a className="contact" href="https://www.vian.ai/contact-us/" target="_blank" rel="noopener noreferrer">Contact Us</a>
                 <small className="coindesk">Powered by <a href="https://www.coindesk.com/price/bitcoin" target="_blank" rel="noopener noreferrer" >CoinDesk</a></small>
+                <NavLink className="profile" to="/profile">Profile</NavLink>
+                <NavLink className="home" to="/">Home</NavLink>
             </div>
         )
     }
