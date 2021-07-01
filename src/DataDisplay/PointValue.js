13c568f8f35bf11730efe7c2499c2a263f80539b
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-07-01 16:33:30 -0400

    finished styling graph

--- src/DataDisplay/PointValue.js
+++ src/DataDisplay/PointValue.js
@@ -1,6 +1,8 @@
+import './Graph.css'
+
 function PointValue(props){
     const date = new Date(props.point[0][0]).toLocaleDateString("en-US")
-    return <div>Date: {date} Value: $ {props.point[0][1]}</div>
+    return <div className="pointval">Date:{date} <br></br> Value: ${parseInt(props.point[0][1])}</div>
 }
 
 export default PointValue;
