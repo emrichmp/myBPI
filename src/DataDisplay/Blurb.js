ae6130a7c457397030df51c6ff1ba366e55e2e1f
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-08-03 14:07:18 -0700

    changed the title header

--- src/DataDisplay/Blurb.js
+++ src/DataDisplay/Blurb.js
@@ -3,7 +3,7 @@ import './Blurb.css'
 //Blurb for the page, explains how the app works and what it does
 function Blurb(){
     return <div className="blurb">
-                <h3 className="blurb-header">Company That Rejected Me BitCoin Reference Tool</h3>
+                <h3 className="blurb-header">myBPI - The Historical BitCoin Value Reference Tool</h3>
                 <div className="blurb-words">
                     <h4 className="blurb-sentence">This tool can be used to find information on bitcoin data. Feel free</h4>
                     <h4 className="blurb-sentence">to use this tool to find the current value of bitcoin or </h4>
